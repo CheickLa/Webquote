362a8117de64c1c6a2f3936e91554382cab09f23
yusa-ai <64932650+yusa-ai@users.noreply.github.com>
2023-11-01 17:34:59 +0100

    Create tmp entities/migrations

---
+++ src/Entity/Quote.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuoteRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: QuoteRepository::class)]
+class Quote
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column]
+    private ?float $amount = null;
+
+    #[ORM\ManyToOne(inversedBy: 'quotes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Company $client_id = null;
+
+    #[ORM\OneToOne(mappedBy: 'quote_id', cascade: ['persist', 'remove'])]
+    private ?Invoice $invoice = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getAmount(): ?float
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(float $amount): static
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getClientId(): ?Company
+    {
+        return $this->client_id;
+    }
+
+    public function setClientId(?Company $client_id): static
+    {
+        $this->client_id = $client_id;
+
+        return $this;
+    }
+
+    public function getInvoice(): ?Invoice
+    {
+        return $this->invoice;
+    }
+
+    public function setInvoice(Invoice $invoice): static
+    {
+        // set the owning side of the relation if necessary
+        if ($invoice->getQuoteId() !== $this) {
+            $invoice->setQuoteId($this);
+        }
+
+        $this->invoice = $invoice;
+
+        return $this;
+    }
+}
