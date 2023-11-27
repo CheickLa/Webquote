52711a48155a9f9406714f57fddf3366542beb12
cheickLa <cheicklanick@gmail.com>
2023-11-27 11:03:52 +0100

    registrer avec attente  d'essai du statut legal

--- src/Controller/RegistrerController.php
+++ src/Controller/RegistrerController.php
@@ -4,15 +4,33 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\CompanyType;
+use App\Entity\Company;
+
 
 class RegistrerController extends AbstractController
 {
+    // Methode pour l'inscription
     #[Route('/registrer', name: 'app_registrer')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $company = new Company();
+
+        $form = $this->createForm(CompanyType::class, $company);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $entityManager->persist($company);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Bravo, vous venez de rejoindre la famille WebQuote !');
+        }
+
         return $this->render('registrer/index.html.twig', [
-            'controller_name' => 'RegistrerController',
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/CompanyType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Company;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CompanyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('siret')
+            ->add('name')
+            // ->add('legal_status')
+            ->add('sector')
+            ->add('address')
+            ->add('phone_number')
+            ->add('sales')
+            ->add('role')
+            ->add('email')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Company::class,
+        ]);
+    }
+}
