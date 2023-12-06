8ab7e795f3919cc3a49038c57ffb53a499ace3ed
Ryan Malonzo <64932650+yusa-ai@users.noreply.github.com>
2023-12-06 22:26:43 +0100

    Add remaining fixtures

---
+++ src/DataFixtures/ServiceCategoryFixtures.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\ServiceCategory;
+use App\Entity\Agency;
+use App\DataFixtures\AgencyFixtures;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class ServiceCategoryFixtures extends Fixture implements DependentFixtureInterface
+{
+  public function load(ObjectManager $manager): void
+  {
+    $agencies = $manager->getRepository(Agency::class)->findAll();
+
+    $categories = array(
+      "Conception de site web",
+      "Développement back-end",
+      "Optimisation SEO",
+      "Sécurité web",
+      "Maintenance et support",
+      "Développement mobile",
+      "Design graphique",
+      "Analyse de données",
+      "Formation en ligne",
+      "Consultation en stratégie numérique"
+    );
+
+    for ($i = 0; $i < 50; $i++) {
+      $agency = (new ServiceCategory())
+        ->setName($categories[array_rand($categories)])
+        ->setAgency($agencies[array_rand($agencies)]);
+      $manager->persist($agency);
+    }
+
+    $manager->flush();
+  }
+
+  public function getDependencies(): array
+  {
+    return [
+      AgencyFixtures::class,
+    ];
+  }
+}
---
+++ src/DataFixtures/ServiceFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Service;
+use App\Entity\ServiceCategory;
+use App\DataFixtures\ServiceCategoryFixtures;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class ServiceFixtures extends Fixture implements DependentFixtureInterface
+{
+  public function load(ObjectManager $manager): void
+  {
+    $faker = \Faker\Factory::create('fr_FR');
+
+    $serviceCategories = $manager->getRepository(ServiceCategory::class)->findAll();
+
+    for ($i = 0; $i < 100; $i++) {
+      $agency = (new Service())
+        ->setName($faker->sentence(3, true))
+        ->setPrice($faker->randomFloat(2, 1, 1000))
+        ->setServiceCategory($faker->randomElement($serviceCategories));
+      $manager->persist($agency);
+    }
+
+    $manager->flush();
+  }
+
+  public function getDependencies(): array
+  {
+    return [
+      ServiceCategoryFixtures::class,
+    ];
+  }
+}
