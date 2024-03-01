a9bab303399194300b81daaf78478d2951e162e6
CheickLa <92098641+CheickLa@users.noreply.github.com>
2024-03-02 00:49:44 +0100

    Wq 32 - Admin ayant accès à toutes les données de toutes les agences (#50)
    * héritages de role
    
    * Mise en place Admin ayant accès à toutes les données de toutes les agences
    
    * Retour au compte admin après usurpation d'un compte user + clean code
    
    * Correction parce qu'une facture n'est pas forcément payée
    
    * Fix header bug

--- src/Controller/SettingsController.php
+++ src/Controller/SettingsController.php
@@ -2,15 +2,40 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\AgencyRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SettingsController extends AbstractController
 {
     #[Route('/dashboard/settings', name: 'app_settings')]
-    public function index(): Response
+    public function index(Request $request, AgencyRepository $agencyRepository): Response
     {
-        return $this->render('settings/index.html.twig');
+        $agencies = $agencyRepository->findAll();
+        $selectedAgencyId = $request->query->get('id');
+
+        if ($selectedAgencyId) {
+            if ($this->isGranted('ROLE_ADMIN')) {
+                $selectedAgency = $agencyRepository->find($selectedAgencyId);
+
+                if ($selectedAgency) {
+                    $user = $selectedAgency->getUsers()->first();
+
+                    if ($user) {
+                        $email = $user->getEmail();
+
+                        return $this->redirectToRoute('app_dashboard', ['_switch_user' => $email]);
+                    }
+                }
+            } else {
+                throw new AccessDeniedException('Vous n\'avez pas les permissions nécessaires pour effectuer cette action.');
+            }
+        }
+
+        return $this->render('settings/index.html.twig', [
+            'agencies' => $agencies,
+        ]);
     }
 }
